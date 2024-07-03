2494e2de1ece912be984f9d6e57d340e09a1f98e
James Dillon <jamesdillonii@gmail.com>
2024-07-03 15:53:54 -0500

    Add create database placeholder function and add new script

---
+++ scripts/convertSpectraToText.m
@@ -0,0 +1,11 @@
+% function convertSpectraToText(channelArray, intensityArray)
+
+% input will be channelArray, intensityArray both many columns, 1 row
+
+% convert channelArray to comma separted text string
+    % 10, 10.100, 10.200, 10.300, .... to end
+
+% convert intensityArray to comma text string
+    % 1165700, 0, 0, 0, 0, 0, ....
+
+% return two strings channelString, intensityString
