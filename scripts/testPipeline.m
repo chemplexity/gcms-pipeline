423a02994951dd373b173e5d376625bbaf29dcc2
James Dillon <jamesdillonii@gmail.com>
2024-08-13 19:43:39 -0500

    Add script to debug pipeline steps

---
+++ scripts/testPipeline.m
@@ -0,0 +1,70 @@
+%% Import data
+data = ImportAgilent();
+
+%% Validate data (remove non-MS files)
+data = validateData(data);
+
+%% Plot data (initialize)
+idx = 1;
+
+plot(data(idx).time, data(idx).intensity(:,1));
+
+%% Plot data (increment index)
+idx = idx + 1;
+
+if idx > length(data)
+    idx = 1;
+end
+
+plot(data(idx).time, data(idx).intensity(:,1));
+fprintf([num2str(idx), ': ', data(idx).sample_name, '\n']);
+
+%% Preprocess data
+startIndex = [];
+endIndex = [];
+
+data = preprocessData(data, ...
+    'startIndex', startIndex, ...
+    'endIndex', endIndex);
+
+%% Detect peaks
+data = detectPeaksInData(data, ...
+    'startIndex', startIndex, ...
+    'endIndex', endIndex, ...
+    'minPeakHeight', 2E4);
+
+%% Plot peaks (initialize)
+idx = 1;
+
+plotPeaksInData(data, idx);
+
+%% Plot peaks (increment index)
+idx = idx + 1;
+
+if idx > length(data)
+    idx = 1;
+end
+
+plotPeaksInData(data, idx);
+
+%% Load library
+libraryFile = './examples/library/GCMS DB-Public-KovatsRI-VS3.msp';
+library = ImportNIST('file', libraryFile);
+
+%% Cleanup library
+library = CleanupLibrary(library, ...
+    'min_points', 5, ...
+    'min_mz', 50, ...
+    'max_mz', 600);
+
+%% Perform spectral matching
+startIndex = [];
+endIndex = [];
+
+data = performSpectralMatch(data, library,...
+    'minScore', 50, ...
+    'startIndex', startIndex, ...
+    'endIndex', endIndex, ...
+    'minMz', 50, ...
+    'maxMz', 600);
+
